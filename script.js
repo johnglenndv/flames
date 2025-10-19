3770cf04f3a1bcde64d4506c22504c23c32d688a
John Glenn De Vera <114321368+johnglenndv@users.noreply.github.com>
2025-10-19 12:46:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function registerUser(event) {
     role: form.role.value
   };
 
-  const res = await fetch("${API_BASE}/register", {
+  const res = await fetch("https://flames-server.onrender.com/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
